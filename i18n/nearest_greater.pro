ec05aecff34ea4c840aae1ede503dfad334567d5
Florian Neukirchen <mail@riannek.de>
2024-03-24 17:50:13 +0100

    i18n

---
+++ i18n/nearest_greater.pro
@@ -0,0 +1,2 @@
+SOURCES = ../nearest_greater.py ../nearest_greater_algorithm.py ../nearest_greater_provider.py
+TRANSLATIONS = nearest_greater_de.ts
---
+++ i18n/nearest_greater_de.ts
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>NearestGreaterAlgorithm</name>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="90"/>
+        <source>Input layer</source>
+        <translation>Eingabelayer</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="99"/>
+        <source>Field with values to compare</source>
+        <translation>Feld, dessen Werte verglichen werden sollen</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="109"/>
+        <source>1 Mio.</source>
+        <translation>1 Mio.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="110"/>
+        <source>Max distance + 1</source>
+        <translation>Max. Entfernung + 1</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="114"/>
+        <source>Choose a distance value for the greatest feature</source>
+        <translation>Entfernungswert für Feature mit größtem Wert</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="124"/>
+        <source>Name or ID to identify nearest greater neighbor</source>
+        <translation>Name oder ID um nächst größeren Nachbar zu Identifizieren</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="132"/>
+        <source>Calculate distance (m) on ellipsoid (WGS84)</source>
+        <translation>Berechne Entfernung (m) auf Ellipsoid (WGS84)</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="141"/>
+        <source>Keep features with NULL value in output layer</source>
+        <translation>Behalte Features mit Wert NULL im Ausgabelayer</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="148"/>
+        <source>Name or ID of Feature with max value</source>
+        <translation>Name oder ID mit Maximalwert</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="154"/>
+        <source>Number of features that have been processed.</source>
+        <translation>Anzahl der verarbeiteten Features.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="161"/>
+        <source>Number of ignored features with NULL values.</source>
+        <translation>Anzahl der ignorierten Features mit Wert NULL.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="167"/>
+        <source>Smallest delta.</source>
+        <translation>Kleinste Differenz.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="173"/>
+        <source>Largest calculated delta.</source>
+        <translation>Größte berechnete Differenz.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="179"/>
+        <source>Mean of calculated delta.</source>
+        <translation>Mittelwert der berechneten Differenz.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="185"/>
+        <source>First quartile of delta.</source>
+        <translation>Erstes Quartil der Differenz.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="191"/>
+        <source>Second quartile (median) of delta.</source>
+        <translation>Zweites Quartil der Differenz.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="197"/>
+        <source>Third quartile of delta.</source>
+        <translation>Drittes Quartil der Differenz.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="204"/>
+        <source>Smallest distance.</source>
+        <translation>Kleinste Entfernung.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="210"/>
+        <source>Largest calculated distance.</source>
+        <translation>Größte berechnete Entfernung.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="216"/>
+        <source>Mean of calculated distances.</source>
+        <translation>Mittelwert der berechneten Entfernungen.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="222"/>
+        <source>First quartile of distances.</source>
+        <translation>Erstes Quartil der Entfernungen.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="228"/>
+        <source>Second quartile (median) of distances.</source>
+        <translation>Zweites Quartil der Entfernungen.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="234"/>
+        <source>Third quartile of distances.</source>
+        <translation>Drittes Quartil der Entfernungen.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="245"/>
+        <source>Output Nearest Greater</source>
+        <translation>Ausgabe Nächster Größerer</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="252"/>
+        <source>Lines Nearest Greater</source>
+        <translation>Linien Nächster Größerer</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="313"/>
+        <source>WARNING: The input is in a geographic CRS and you choosed to calculate distance on projected plane. Consider to calculate distance on ellipsoid or a layer in projected CRS.</source>
+        <translation>WARNUNG: Der Eingabelayer ist in einem geographischen CRS und Sie wollen die Entfernung auf der projezierten Ebene berechnen. Sie sollten besser die Entfernung auf dem Ellipsoid berechnen oder die Ebene reprojezieren.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="329"/>
+        <source>Error: Converting the field to floating point value failed with Value Error: {e.args[0]}
+</source>
+        <translation>Fehler: Umwandlung des Felds zu Fließkommazahl scheitert mit Value Error: {e.args[0]}
+</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="332"/>
+        <source>WARNING: The fields will be compared as type: {0}</source>
+        <translation>WARNUNG: Die Felder werden als Datentyp {0} verglichen</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="359"/>
+        <source>{0} of {1} features have NULL as value and are ignored.</source>
+        <translation>{0} von {1} Features haben Wert NULL und werden ignoriert.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="362"/>
+        <source>WARNING: There are non unique names in the selected ID/name field, it might be better to use another field.</source>
+        <translation>WARNING: Es gibt Duplikate im Feld name/ID, es wäre besser, ein anderes Feld zu wählen.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="364"/>
+        <source>WARNING: There are NULL values in the selected ID/name field, it might be better to use another field.</source>
+        <translation>WARNUNG: Es gibt NULL-Werte im gewählten Feld ID/Name, es wäre besser, ein anderes Feld zu wählen.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="452"/>
+        <source>Processing finished.</source>
+        <translation>Verarbeitung beendet.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="456"/>
+        <source>Add features with null value.</source>
+        <translation>Füge Features mit Wert Null hinzu.</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="482"/>
+        <source>
+Calculate statistics:</source>
+        <translation>
+Berechne Statistik:</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="488"/>
+        <source>
+Delta:</source>
+        <translation>
+Differenz:</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="501"/>
+        <source>
+Distance:</source>
+        <translation>
+Entfernung:</translation>
+    </message>
+    <message>
+        <location filename="../nearest_greater_algorithm.py" line="549"/>
+        <source>nearest with greater value</source>
+        <translation>Nächster Nachbar mit größerem Wert</translation>
+    </message>
+</context>
+<context>
+    <name>NearestGreaterProvider</name>
+    <message>
+        <location filename="../nearest_greater_provider.py" line="77"/>
+        <source>Nearest greater</source>
+        <translation>Nächster größerer</translation>
+    </message>
+</context>
+</TS>
